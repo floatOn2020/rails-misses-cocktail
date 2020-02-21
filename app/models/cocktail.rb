69d89d3b5e98f091c35b83dd888cd33066e364c4
Bianca Herbst <b.herbst@posteo.de>
2020-02-21 16:24:59 +0100

    add banner

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,7 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
 end
 
 # validates :name, uniqueness: true, presence: true
